b9405bdf0a23d273b88dd40171b5352c70a31cd5
Bruno Lopes Chagas <brunolopeschagas@gmail.com>
2023-12-05 10:53:18 -0300

    pág 72 - getAllPage

--- src/main/java/br/com/brunochagas/userapi/userapi/UserService.java
+++ src/main/java/br/com/brunochagas/userapi/userapi/UserService.java
@@ -4,6 +4,8 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
@@ -22,30 +24,63 @@ public class UserService {
                 .collect(Collectors.toList());
     }
 
-    public UserDTO findById(long userId){
+    public UserDTO findById(long userId) {
         User usuario = userRepository.findById(userId)
-        .orElseThrow(()->new RuntimeException("User not found"));
+                .orElseThrow(() -> new RuntimeException("User not found"));
         return UserDTO.convert(usuario);
     }
 
-    public UserDTO save(UserDTO userDTO){
+    public UserDTO save(UserDTO userDTO) {
         userDTO.setDadaCadastro(LocalDateTime.now());
         User user = userRepository.save(User.convert(userDTO));
         return UserDTO.convert(user);
     }
 
-    public UserDTO delete(long userId){
-        User user = userRepository.findById(userId).orElseThrow(()-> new RuntimeException());
+    public UserDTO delete(long userId) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException());
         userRepository.delete(user);
         return UserDTO.convert(user);
     }
 
-    public UserDTO findByCpf(String cpf){
+    public UserDTO findByCpf(String cpf) {
         User user = userRepository.findByCpf(cpf);
-        if(user != null){
+        if (user != null) {
             return UserDTO.convert(user);
         }
-        return null;        
+        return null;
+    }
+
+    public List<UserDTO> queryByName(String name) {
+        List<User> usuarios = userRepository.queryByNomeLike(name);
+        return usuarios
+                .stream()
+                .map(UserDTO::convert)
+                .collect(Collectors.toList());
+    }
+
+    public UserDTO editUser(Long userId, UserDTO userDTO) {
+        User user = userRepository
+                .findById(userId)
+                .orElseThrow(() -> new RuntimeException());
+
+        if (userDTO.getEmail() != null && !user.getEmail().equals(userDTO.getEmail())) {
+            user.setEmail(userDTO.getEmail());
+        }
+        if (userDTO.getTelefone() != null && !user.getTelefone().equals(userDTO.getTelefone())) {
+            user.setTelefone(userDTO.getTelefone());
+        }
+        if (userDTO.getEndereço() != null && !user.getEndereco().equals(userDTO.getEndereço())) {
+            user.setEndereco(userDTO.getEndereço());
+        }
+
+        user = userRepository.save(user);
+        return UserDTO.convert(user);
+    }
+
+    public Page<UserDTO> getAllPage(Pageable page) {
+        Page<User> users = userRepository.findAll(page);
+        return users
+                .map(UserDTO::convert);
     }
 
 }
