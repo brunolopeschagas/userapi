73c7e93dcc8b1cb2eee39af954892a81c7d3e7e6
Bruno Lopes Chagas <brunolopeschagas@gmail.com>
2023-12-05 11:13:04 -0300

    pág 74

--- src/main/java/br/com/brunochagas/userapi/userapi/UserController.java
+++ src/main/java/br/com/brunochagas/userapi/userapi/UserController.java
@@ -1,80 +1,57 @@
 package br.com.brunochagas.userapi.userapi;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.http.HttpStatus;
 
-import jakarta.annotation.PostConstruct;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/user")
+@RequiredArgsConstructor
 public class UserController {
 
-    public static List<UserDTO> usuarios = new ArrayList<UserDTO>();
+    private final UserService userService;
 
     @GetMapping
     public List<UserDTO> getUsers() {
-        return usuarios;
+        return userService.getAll();
     }
 
-    @GetMapping("/{cpf}")
-    public UserDTO getUsersFiltro(@PathVariable String cpf) {
-        return usuarios
-                .stream()
-                .filter(userDTO -> userDTO.getCpf().equals(cpf))
-                .findFirst()
-                .orElseThrow(() -> new RuntimeException("User not found"));
+    @GetMapping("/{id}")
+    public UserDTO findById(@PathVariable Long id) {
+        return userService.findById(id);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public UserDTO inserir(@RequestBody @Valid UserDTO userDTO){
-        userDTO.setDadaCadastro(LocalDateTime.now());
-        usuarios.add(userDTO);
-        return userDTO;
+    public UserDTO inserir(@RequestBody @Valid UserDTO userDTO) {
+        return userService.save(userDTO);
     }
 
-    @DeleteMapping("/{cpf}")
-    public boolean remover(@PathVariable String cpf){
-        return usuarios.removeIf(userDTO -> userDTO.getCpf().equals(cpf));
+      @GetMapping("/{cpf}/cpf")
+    public UserDTO findByCpf(@PathVariable String cpf) {
+        return userService.findByCpf(cpf);
     }
 
-    @PostConstruct
-    public void initiateList() {
-        UserDTO userDTO = new UserDTO();
-        userDTO.setNome("Eduardo");
-        userDTO.setCpf("123");
-        userDTO.setEndereço("Rua a");
-        userDTO.setEmail("eduardo@email.com");
-        userDTO.setTelefone("1 234-3454");
-        userDTO.setDadaCadastro(LocalDateTime.now());
-        UserDTO userDTO2 = new UserDTO();
-        userDTO2.setNome("Luiz");
-        userDTO2.setCpf("456");
-        userDTO2.setEndereço("Rua b");
-        userDTO2.setEmail("luiz@email.com");
-        userDTO2.setTelefone("1234-3454");
-        userDTO2.setDadaCadastro(LocalDateTime.now());
-        UserDTO userDTO3 = new UserDTO();
-        userDTO3.setNome("Bruna");
-        userDTO3.setCpf("678");
-        userDTO3.setEndereço("Rua c");
-        userDTO3.setEmail("bruna@email.com");
-        userDTO3.setTelefone("1234-3454");
-        userDTO3.setDadaCadastro(LocalDateTime.now());
-        usuarios.add(userDTO);
-        usuarios.add(userDTO2);
-        usuarios.add(userDTO3);
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) throws RuntimeException {
+        userService.delete(id);
+    }
+
+    @GetMapping("/search")
+    public List<UserDTO> queryByName(@RequestParam(name = "nome", required = true) String nome) {
+        return userService.queryByName(nome);
     }
 }
